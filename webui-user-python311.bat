e35fc689e9ffe97620cba8505f1e51197bc19b56
githubcto <165018162+githubcto@users.noreply.github.com>
2026-03-08 00:06:38 +0900

    add webui-user-python311.bat for python3.11
    add webui-user-python311.bat for python3.11

---
+++ webui-user-python311.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd /d "%~dp0"
+
+set PYTHON=py.exe -3.11
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS=
+
+@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% --models-dir D:/AI/models
+
+call webui.bat
